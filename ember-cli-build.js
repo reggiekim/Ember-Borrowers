09269ff182826fdac135db213d58587e66f0b0c5
Reggie Kim <reggiekim@gmail.com>
2016-11-18 14:33:50 -0500

    add basscss

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,8 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('vendor/basscss.min.css');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
