6677cc42f25296079bc171a147bf948c3ab58335
Reggie Kim <reggiekim@gmail.com>
2016-11-18 10:20:11 -0500

    add routes, views, controller actions for create and read Friends

--- app/router.js
+++ app/router.js
@@ -7,7 +7,17 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('friends');
+  this.route('friends', function() {
+    this.route('new');
+
+    this.route('show', {
+      path: ':friend_id'
+    });
+
+    this.route('edit', {
+      path: ':friend_id/edit'
+    });
+  });
 });
 
 export default Router;
