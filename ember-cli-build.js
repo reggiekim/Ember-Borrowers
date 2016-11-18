2b343a0e6b8680896d5df1ffb1d99e378750a71e
Reggie Kim <reggiekim@gmail.com>
2016-11-18 14:41:11 -0500

    add fontello

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,23 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('vendor/fontello/fontello.css');
+  app.import('vendor/fontello/font/fontello.ttf', {
+  destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/fontello.eot', {
+  destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/fontello.svg', {
+  destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/fontello.woff', {
+  destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/fontello.woff2', {
+  destDir: 'assets/font'
+  });
+
   app.import('vendor/basscss.min.css');
 
   // Use `app.import` to add additional libraries to the generated
