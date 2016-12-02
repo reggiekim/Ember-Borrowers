b4477e5bdfd87e092e8f92034d5d713015ef0b3c
Reggie Kim <reggiekim@gmail.com>
2016-12-02 15:45:54 -0500

    consume MomentJS date helper via Browserify rather than Bower

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -26,7 +26,7 @@ module.exports = function(defaults) {
 
   app.import('vendor/basscss.min.css');
 
-  app.import('bower_components/moment/moment.js');
+
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
