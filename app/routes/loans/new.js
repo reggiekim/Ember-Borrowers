5254b26d82e3b9f96d6f45932cc476e1989fdf95
Reggie Kim <reggiekim@gmail.com>
2016-12-02 11:52:59 -0500

    add resetController hooks to reset the store for unsaved data in Friends, Articles, Loan forms

--- app/routes/loans/new.js
+++ app/routes/loans/new.js
@@ -5,5 +5,20 @@ export default Ember.Route.extend({
     return this.store.createRecord('loan', {
       friend: this.modelFor('friends/show')
     });
+  },
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      // We grab the model from the controller
+      //
+      var model = controller.get('model');
+      // Because we are leaving the Route we verify if the model is in
+      // 'isNew' state, which means it wasn't saved to the backend.
+      //
+      if (model.get('isNew')) {
+        // We call DS#destroyRecord() which removes it from the store
+        //
+        model.destroyRecord();
+      }
+    }
   }
 });
