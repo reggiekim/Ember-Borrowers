7dbcd20f041fe89ee2e4998d53d6ca26aad75de0
Reggie Kim <reggiekim@gmail.com>
2016-12-08 14:18:58 -0500

    revert to not consuming fill-murray plugin package

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('friend');
+    // return this.store.findAll('friend');
+    //
+    // We now use store.query and pass include in the options
+    // below we include "loans", which returns JSON of all friends, and includes loans related to each friend
+    return this.store.query('friend', {include: 'loans'});
   }
 });
