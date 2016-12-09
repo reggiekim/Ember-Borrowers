b2b2d956c8ce90d09dad4f3b8641575fba73c09f
Reggie Kim <reggiekim@gmail.com>
2016-12-09 17:19:17 -0500

    add indentation

--- app/router.js
+++ app/router.js
@@ -10,13 +10,13 @@ Router.map(function() {
   this.route('friends', function() {
     this.route('new');
 
-  this.route('show', {
-    path: ':friend_id'
-  }, function() {
-    this.route('loans', {resetNamespace: true}, function() {
-      this.route('new');
+    this.route('show', {
+      path: ':friend_id'
+    }, function() {
+      this.route('loans', {resetNamespace: true}, function() {
+        this.route('new');
+      });
     });
-  });
 
     this.route('edit', {
       path: ':friend_id/edit'
